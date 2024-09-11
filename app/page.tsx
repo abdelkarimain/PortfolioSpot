3276ece49bd5f25cbc6a78681af53a8e364432b5
Abdelkarim Ain <162830944+abdelkarimain@users.noreply.github.com>
2024-09-11 14:38:59 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { Link } from "lucide-react";
-
 export default function Home() {
   return (
     <>
@@ -10,36 +6,7 @@ export default function Home() {
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
             Welcome to Portfolio Spot
           </h1>
-          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-            Where you can find some of the best developer's portfolios
-          </p>
-          <div className="flex flex-col gap-4 justify-center sm:flex-row">
-            <Link
-              href="/blog"
-              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
-            >
-              View All
-            </Link>
-            <Link
-              href={"https://github.com/abdelkarimain"}
-              target="_blank"
-              className={cn(
-                buttonVariants({ variant: "outline", size: "lg" }),
-                "w-full sm:w-fit"
-              )}
-            >
-              GitHub
-            </Link>
-          </div>
         </div>
-      </section>
-      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
-        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
-          Latest Posts
-        </h2>
-        <ul className="flex flex-col">
-          <li>hi</li>
-        </ul>
       </section>
     </>
   );
