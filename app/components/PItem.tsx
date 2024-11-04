c7e5a306244b4b1194bd34bcf6a4b8111d77750f
Abdelkarim Ain <162830944+abdelkarimain@users.noreply.github.com>
2024-11-04 16:41:08 +0100

    fixing an error with stack badge color

--- app/components/PItem.tsx
+++ app/components/PItem.tsx
@@ -2,24 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import img from "../assets/tz3oe8lbftjvobsub95l.png";
 
-
 export default function PItem() {
   const bgColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-  function darkenColor(color: string, factor: number) {
-    const rgba = color.match(/\d+(\.\d+)?/g)?.map(Number);
-
-    if (rgba && (rgba.length === 3 || rgba.length === 4)) {
-      const [r, g, b, a = 1] = rgba;
-      const darkenedR = Math.max(0, Math.floor(r * (1 - factor)));
-      const darkenedG = Math.max(0, Math.floor(g * (1 - factor)));
-      const darkenedB = Math.max(0, Math.floor(b * (1 - factor)));
-
-      return `rgba(${darkenedR}, ${darkenedG}, ${darkenedB}, ${a})`;
-    }
-
-    return color;
-  }
 
   return (
     <Link
@@ -42,7 +26,9 @@ export default function PItem() {
         </div>
       </div>
       <div className="p-3">
-        <span className="font-semibold text-2xl font-mono drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Abdelkarim Ain</span>
+        <span className="font-semibold text-2xl font-mono drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+          Abdelkarim Ain
+        </span>
 
         {/* <div className="mt-2 flex items-center  gap-x-5">
           <div className="flex items-center gap-x-2">
